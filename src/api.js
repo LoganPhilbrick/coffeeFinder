1846bb8136ed263c4fca609e0bee558d53652987
Logan Philbrick <loganphilbrick99@gmail.com>
2023-04-09 21:16:31 -0400

    const {header} = response_object

--- src/api.js
+++ src/api.js
@@ -2,7 +2,10 @@ export const fetchCoffeeData = (apiKey, latitude, longitude) => {
   const options = {
     method: "GET",
     mode: "cors",
-    headers: { accept: "application/json", Authorization: `Bearer ${apiKey}` },
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
   };
   if (window.location.hostname === "localhost") {
     return fetch(`/businesses/search?latitude=${latitude}&longitude=${longitude}&categories=coffee&sort_by=best_match&limit=20`, options).then((res) => res.json());

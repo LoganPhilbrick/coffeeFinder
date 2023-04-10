8d2ac0bd4033c91a1d7ed0da45374d938a709797
Logan Philbrick <loganphilbrick99@gmail.com>
2023-04-09 20:43:32 -0400

    push that up

--- src/api.js
+++ src/api.js
@@ -3,6 +3,10 @@ export const fetchCoffeeData = (apiKey, latitude, longitude) => {
     method: "GET",
     headers: { accept: "application/json", Authorization: `Bearer ${apiKey}` },
   };
-
-  return fetch(`/businesses/search?latitude=${latitude}&longitude=${longitude}&categories=coffee&sort_by=best_match&limit=20`, options).then((res) => res.json());
+  if (window.location.hostname === "localhost") {
+    return fetch(`/businesses/search?latitude=${latitude}&longitude=${longitude}&categories=coffee&sort_by=best_match&limit=20`, options).then((res) => res.json());
+  }
+  if (window.location.hostname !== "localhost") {
+    return fetch(`https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}&categories=coffee&sort_by=best_match&limit=20`, options).then((res) => res.json());
+  }
 };

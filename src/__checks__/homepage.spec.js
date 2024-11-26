62e761808e90d90ca544aa52ad331904567c3c14
WideLogan <logan@widecorpus.com>
2024-11-25 19:21:16 -0500

    finished homepage spec file

--- src/__checks__/homepage.spec.js
+++ src/__checks__/homepage.spec.js
@@ -1,7 +1,9 @@
 import { expect, test } from "@playwright/test";
 
+test.use({ channel: "chrome" });
+
 test("Visit home page", async ({ page }) => {
-  const response = await page.goto("https://checklyhq.com");
+  const response = await page.goto("https://coffeecompass.vercel.app");
 
   // Test that the response did not fail
   expect(response.status()).toBeLessThan(400);

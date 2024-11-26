c1a5ba0cd68ca174917d589d85d22ec0ad6fd252
WideLogan <logan@widecorpus.com>
2024-11-25 19:34:59 -0500

    commecnted out checks test/added to gitignore

--- checkly.config.js
+++ checkly.config.js
@@ -1,27 +1,27 @@
-import { defineConfig } from "checkly";
-import { Frequency } from "checkly/constructs";
+// import { defineConfig } from "checkly";
+// import { Frequency } from "checkly/constructs";
 
-const config = defineConfig({
-  projectName: "Website Monitoring",
-  logicalId: "website-monitoring-1",
-  repoUrl: "https://github.com/LoganPhilbrick/coffeeFinder",
-  checks: {
-    activated: true,
-    muted: false,
-    runtimeId: "2022.10",
-    frequency: Frequency.EVERY_5M,
-    locations: ["us-east-1", "eu-west-1"],
-    tags: ["website", "api"],
-    checkMatch: "**/__checks__/*.check.js",
-    browserChecks: {
-      frequency: Frequency.EVERY_10M,
-      testMatch: "browsers/**/*.spec.js",
-    },
-  },
-  cli: {
-    runLocation: "eu-east-1",
-    privateRunLocation: "private-dc1",
-  },
-});
+// const config = defineConfig({
+//   projectName: "Website Monitoring",
+//   logicalId: "website-monitoring-1",
+//   repoUrl: "https://github.com/LoganPhilbrick/coffeeFinder",
+//   checks: {
+//     activated: true,
+//     muted: false,
+//     runtimeId: "2022.10",
+//     frequency: Frequency.EVERY_5M,
+//     locations: ["us-east-1", "eu-west-1"],
+//     tags: ["website", "api"],
+//     checkMatch: "**/__checks__/*.check.js",
+//     browserChecks: {
+//       frequency: Frequency.EVERY_10M,
+//       testMatch: "browsers/**/*.spec.js",
+//     },
+//   },
+//   cli: {
+//     runLocation: "eu-east-1",
+//     privateRunLocation: "private-dc1",
+//   },
+// });
 
-module.exports = config;
+// module.exports = config;
--- src/__checks__/homepage.spec.js
+++ src/__checks__/homepage.spec.js
@@ -1,10 +1,10 @@
-import { expect, test } from "@playwright/test";
+// import { expect, test } from "@playwright/test";
 
-test.use({ channel: "chrome" });
+// test.use({ channel: "chrome" });
 
-test("Visit home page", async ({ page }) => {
-  const response = await page.goto("https://coffeecompass.vercel.app");
+// test("Visit home page", async ({ page }) => {
+//   const response = await page.goto("https://coffeecompass.vercel.app");
 
-  // Test that the response did not fail
-  expect(response.status()).toBeLessThan(400);
-});
+//   // Test that the response did not fail
+//   expect(response.status()).toBeLessThan(400);
+// });

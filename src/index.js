40b63f4369573e98345dfb15d2952db1c2c8b644
WideLogan <logan@widecorpus.com>
2024-11-25 20:07:06 -0500

    removed fonts from header as they are installed as npm pckg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Details from "./pages/Details";
-import "@fontsource/roboto";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const Router = createBrowserRouter([
   {
